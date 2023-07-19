9cf7e5b4f83b75f70a839cb110d1316a02a37d43
Kalyana-Sudheer-p <113004832+Kalyana-Sudheer-p@users.noreply.github.com>
2023-07-19 18:07:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
+
+
 import Login from "./components/login_component";
 import SignUp from "./components/signup_component";
 import UserDetails from "./components/userDetails";
